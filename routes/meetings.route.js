4194123c58900a479ac15b40c74086e698a03516
Anoh Abbah <anohabbah@gmail.com>
2019-05-18 13:56:57 +0200

    test(meeting-tests): PUT, DELETE

--- routes/meetings.route.js
+++ routes/meetings.route.js
@@ -15,4 +15,31 @@ router.post('/', async (req, res) => {
   res.status(201).json(meeting);
 });
 
+router.get('/:projectId', async (req, res) => {
+  const id = parseInt(req.params['projectId'], 10);
+
+  const meeting = await Meeting.findByPk(id);
+
+  res.status(200).json(meeting);
+});
+
+router.put('/:projectId', async (req, res) => {
+  const id = parseInt(req.params['projectId'], 10);
+  const values = req.body;
+
+  const meeting = await Meeting.findByPk(id);
+  meeting.update(values);
+
+  res.status(200).json(meeting);
+});
+
+router.delete('/:projectId', async (req, res) => {
+  const id = parseInt(req.params['projectId'], 10);
+
+  const meeting = await Meeting.findByPk(id);
+  await meeting.destroy();
+
+  res.status(200).json(meeting);
+});
+
 module.exports = router;
--- test/integration/meetings.test.js
+++ test/integration/meetings.test.js
@@ -9,11 +9,13 @@ let meeting;
 
 describe('Meetings', () => {
   let attributes;
+  let url;
   const apiURL = '/api/meetings';
 
   beforeEach(async () => {
     await sequelize.sync({ force: true });
 
+    url = undefined;
     attributes = {
       title: faker.lorem.sentence(),
       description: faker.lorem.paragraph()
@@ -54,6 +56,68 @@ describe('Meetings', () => {
 
       const meetings = await Meeting.findAll();
       expect(parse(meetings)).toEqual(expect.arrayContaining([res.body]));
+    });
+  });
+
+  describe(`GET ${apiURL}/:meetingId`, () => {
+    const exec = async () => await request(app).get(url);
+
+    it('should respond 200', async () => {
+      url = `${apiURL}/${meeting.id}`;
+      const res = await exec();
+      expect(res.statusCode).toBe(200);
+    });
+
+    it('should retrieve meeting details', async () => {
+      url = `${apiURL}/${meeting.id}`;
+      const res = await exec();
+      expect(res.body).toEqual(expect.objectContaining(parse(meeting)));
+    });
+  });
+
+  describe(`PUT ${apiURL}/:projectId`, () => {
+    let data;
+
+    const exec = async () =>
+      await request(app)
+        .put(url)
+        .send(data);
+
+    it('should respond 200', async () => {
+      url = `${apiURL}/${meeting.id}`;
+      const res = await exec();
+      expect(res.statusCode).toBe(200);
+    });
+
+    it('should update a meeting', async () => {
+      url = `${apiURL}/${meeting.id}`;
+      data = {
+        title: faker.lorem.sentence(),
+        description: faker.lorem.paragraph()
+      };
+
+      const res = await exec();
+
+      expect(res.body).toEqual(expect.objectContaining(data));
+    });
+  });
+
+  describe(`DELETE ${apiURL}/:projectId`, () => {
+    const exec = async () => request(app).delete(url);
+
+    it('should respond 200', async () => {
+      url = `${apiURL}/${meeting.id}`;
+      const res = await exec();
+      expect(res.statusCode).toBe(200);
+    });
+
+    it('should delete a meeting', async () => {
+      url = `${apiURL}/${meeting.id}`;
+      const res = await exec();
+
+      const meetings = await Meeting.findAll();
+      expect(res.body).toEqual(expect.objectContaining(parse(meeting)));
+      expect(parse(meetings)).toEqual(expect.not.arrayContaining([res.body]));
     });
   });
 });
