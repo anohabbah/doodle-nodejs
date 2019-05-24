980ffa070ccfbf46ff097b70c4ba9cb7be323dc3
Anoh Abbah <anohabbah@gmail.com>
2019-05-24 11:32:16 +0200

    refactor(meeting.route): remove global transaction and put it in a single function

--- routes/meetings.route.js
+++ routes/meetings.route.js
@@ -21,77 +21,75 @@ const { SURVEY_TYPE } = require('../utils/constants.util');
 
 /**
  *
- * @param {Object} transaction
  * @param {string[]} dates
  * @param {number} meetingId
  * @return {Promise<Object>}
  */
-async function createDateSurvey(transaction, dates, meetingId) {
-  const survey = await DateSurvey.create({}, { transaction });
-  for (const timestamp of dates) {
-    await survey.createDate({ timestamp }, { transaction });
-  }
+async function createDateSurvey(dates, meetingId) {
+  return await sequelize.transaction(async transaction => {
+    const survey = await DateSurvey.create({}, { transaction });
+    for (const timestamp of dates) {
+      await survey.createDate({ timestamp }, { transaction });
+    }
 
-  await survey.createSurvey({ meetingId }, { transaction });
+    await survey.createSurvey({ meetingId }, { transaction });
 
-  const d = await survey.getDates({ attributes: ['id', 'timestamp'] });
+    const d = await survey.getDates({ attributes: ['id', 'timestamp'] });
 
-  return Object.assign({}, parse(survey), { dates: parse(d) });
+    return Object.assign({}, parse(survey), { dates: parse(d) });
+  });
 }
 
 /**
  *
- * @param {Object} transaction
  * @param {string[]} locations
  * @param {number} meetingId
  * @return {Promise<Object>}
  */
-async function createLocationSurvey(transaction, locations, meetingId) {
-  const survey = await LocationSurvey.create({}, { transaction });
-  for (const address of locations) {
-    await survey.createLocation({ address }, { transaction });
-  }
+async function createLocationSurvey(locations, meetingId) {
+  return await sequelize.transaction(async transaction => {
+    const survey = await LocationSurvey.create({}, { transaction });
+    for (const address of locations) {
+      await survey.createLocation({ address }, { transaction });
+    }
 
-  await survey.createSurvey({ meetingId }, { transaction });
+    await survey.createSurvey({ meetingId }, { transaction });
 
-  const l = await survey.getLocations({ attributes: ['id', 'address'] });
+    const l = await survey.getLocations({ attributes: ['id', 'address'] });
 
-  return Object.assign({}, parse(survey), { locations: parse(l) });
+    return Object.assign({}, parse(survey), { locations: parse(l) });
+  });
 }
 
 /**
  *
  * @param {string[]} dates
  * @param {string[]} locations
- * @param {Object} transaction
  * @param {number} meetingId
  * @return {Promise<Object>}
  */
-async function createLocationAndDateSurvey(
-  dates,
-  locations,
-  transaction,
-  meetingId
-) {
-  const survey = await LocationAndDateSurvey.create({}, { transaction });
+async function createLocationAndDateSurvey(dates, locations, meetingId) {
+  return await sequelize.transaction(async transaction => {
+    const survey = await LocationAndDateSurvey.create({}, { transaction });
 
-  for (const timestamp of dates) {
-    await survey.createDate({ timestamp }, { transaction });
-  }
+    for (const timestamp of dates) {
+      await survey.createDate({ timestamp }, { transaction });
+    }
 
-  for (const address of locations) {
-    await survey.createLocation({ address }, { transaction });
-  }
+    for (const address of locations) {
+      await survey.createLocation({ address }, { transaction });
+    }
 
-  await survey.createSurvey({ meetingId }, { transaction });
+    await survey.createSurvey({ meetingId }, { transaction });
 
-  const l = await survey.getLocations({ attributes: ['id', 'address'] });
+    const l = await survey.getLocations({ attributes: ['id', 'address'] });
 
-  const d = await survey.getDates({ attributes: ['id', 'timestamp'] });
+    const d = await survey.getDates({ attributes: ['id', 'timestamp'] });
 
-  return Object.assign({}, parse(survey), {
-    locations: parse(l),
-    dates: parse(d)
+    return Object.assign({}, parse(survey), {
+      locations: parse(l),
+      dates: parse(d)
+    });
   });
 }
 
@@ -102,20 +100,22 @@ async function createLocationAndDateSurvey(
  * @param {Object} transaction
  * @return {Promise<Object>}
  */
-async function createMealSurvey(meals, meetingId, transaction) {
-  const survey = await MealSurvey.create({}, { transaction });
+async function createMealSurvey(meals, meetingId) {
+  return await sequelize.transaction(async transaction => {
+    const survey = await MealSurvey.create({}, { transaction });
 
-  const mMeals = [];
-  for (const name of meals) {
-    const meal = await Meal.create({ name }, { transaction });
-    mMeals.push(meal);
-  }
+    const mMeals = [];
+    for (const name of meals) {
+      const meal = await Meal.create({ name }, { transaction });
+      mMeals.push(meal);
+    }
 
-  await survey.setMeals(mMeals);
+    await survey.setMeals(mMeals);
 
-  await survey.createSurvey({ meetingId }, { transaction });
+    await survey.createSurvey({ meetingId }, { transaction });
 
-  return await survey.reload({ include: [{ model: Meal, as: 'meals' }] });
+    return await survey.reload({ include: [{ model: Meal, as: 'meals' }] });
+  });
 }
 
 router['use'](authMiddleware);
@@ -194,48 +194,46 @@ router['delete']('/:meetingId', async (req, res) => {
 // SURVEYS
 
 router['post']('/:meetingId/surveys', async (req, res) => {
-  const survey = await sequelize.transaction(async transaction => {
-    const authUserId = parseInt(req['user'], 10);
-    const surveyType = parseInt(req.body['surveyType'], 10);
-    // const { locations } = req.body;
-    const { meetingId } = req.params;
+  const authUserId = parseInt(req['user'], 10);
+  const surveyType = parseInt(req.body['surveyType'], 10);
+  // const { locations } = req.body;
+  const { meetingId } = req.params;
 
-    const meeting = await Meeting.findByPk(meetingId);
+  const meeting = await Meeting.findByPk(meetingId);
 
-    if (!meeting) throw createNotFoundError('Not Found! Resource not found.');
+  if (!meeting) throw createNotFoundError('Not Found! Resource not found.');
 
-    if (meeting.ownerId !== authUserId)
-      throw createForbiddenError(
-        'Forbidden ! Only meeting owner can add a survey to it.'
-      );
+  if (meeting.ownerId !== authUserId)
+    throw createForbiddenError(
+      'Forbidden ! Only meeting owner can add a survey to it.'
+    );
 
-    switch (surveyType) {
-      case SURVEY_TYPE.DateSurvey: {
-        const { dates } = req.body;
-        return await createDateSurvey(transaction, dates, meetingId);
-      }
-
-      case SURVEY_TYPE.LocationSurvey: {
-        const { locations } = req.body;
-        return await createLocationSurvey(transaction, locations, meetingId);
-      }
-
-      case SURVEY_TYPE.LocationAndDateSurvey: {
-        const { dates, locations } = req.body;
-        return await createLocationAndDateSurvey(
-          dates,
-          locations,
-          transaction,
-          meetingId
-        );
-      }
-
-      case SURVEY_TYPE.MealSurvey: {
-        const { meals } = req.body;
-        return await createMealSurvey(meals, meetingId, transaction);
-      }
+  let survey;
+  switch (surveyType) {
+    case SURVEY_TYPE.DateSurvey: {
+      const { dates } = req.body;
+      survey = await createDateSurvey(dates, meetingId);
+      break;
     }
-  });
+
+    case SURVEY_TYPE.LocationSurvey: {
+      const { locations } = req.body;
+      survey = await createLocationSurvey(locations, meetingId);
+      break;
+    }
+
+    case SURVEY_TYPE.LocationAndDateSurvey: {
+      const { dates, locations } = req.body;
+      survey = await createLocationAndDateSurvey(dates, locations, meetingId);
+      break;
+    }
+
+    case SURVEY_TYPE.MealSurvey: {
+      const { meals } = req.body;
+      survey = await createMealSurvey(meals, meetingId);
+      break;
+    }
+  }
 
   res.status(200).json(survey);
 });
